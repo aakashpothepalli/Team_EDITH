0a898be39ce0700d096a08634d7daa286f532f23
Aakash <aakash@LAPTOP-QS7689OR.localdomain>
2021-09-06 14:20:43 +0530

    working

---
+++ swasth/website/src/components/addPatient.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function AddPatient() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <IconButton className = {classes.menuButton}edge="start"  color="inherit" aria-label="menu"
+                onClick={handleClickOpen}
+              >
+              <AddIcon />
+    </IconButton>
+      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Sound
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <List>
+          <ListItem button>
+            <ListItemText primary="Phone ringtone" secondary="Titania" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText primary="Default notification ringtone" secondary="Tethys" />
+          </ListItem>
+        </List>
+      </Dialog>
+    </div>
+  );
+}
--- swasth/website/src/landing.js
+++ swasth/website/src/landing.js
@@ -1,21 +1,72 @@
-import React  from "react";
+import React,{useEffect}  from "react";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import PatientCard from "./components/patientCard"
+import axios from "axios";
+import { makeStyles } from '@material-ui/core/styles';
+import AddPatient from "./components/addPatient";
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: 20
+  },
+  title: {
+    flexGrow: 1,
+    marginLeft:50
+  },
 
+});
 export default function Landing() {
-  return (
-  <div>
-      <AppBar position="static">
-        <Toolbar>
-            <Typography variant="h6" >
-            Swasth Login
-            </Typography>
-            <Button color="inherit">Login</Button>
-        </Toolbar>
-    </AppBar>
+  let [patients,setPatients] = React.useState([]);
+  async function getPatients() {
 
+    const response = await axios.get('https://team-edith.glitch.me/doctor/listPatients');
+    // console.log(response.data);
+    setPatients(response.data);
+  }  
+  const classes = useStyles();
+
+  useEffect(  () => {
+
+    getPatients()
+  })
+  return (
+  <div >
+      <div className={classes.root}>
+
+        <AppBar position="static">
+          <Toolbar >
+              <Typography variant="h6" className={classes.title} >
+              Swasth Doctor
+              </Typography>
+              <AddPatient/>
+
+          </Toolbar>
+      </AppBar>
+      </div>
+    <div style={{margin:20}}>
+    <List component="nav" aria-label="main mailbox folders">
+        {patients.map(p => 
+        <PatientCard 
+        patientName={p.patientName}
+        patientId = {p.patientId}
+        category = {p.category}
+        description = {p.description??""}
+        bookings = {p.bookings}/>)}
+        {/* <PatientCard patientName= {'aakash'}/> */}
+        {/* <PatientCard />
+        <PatientCard />
+        <PatientCard /> */}
+
+      </List>
+    </div>
   </div>);
 
 }
