ef827c482f86327529a7349d683058dc004bb254
Aakash <aakash@LAPTOP-QS7689OR.localdomain>
2021-09-06 12:12:59 +0530

    init website

---
+++ swasth/website/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from './landing';
+
+function App() {
+  return (
+    <div className="App">
+        <Landing></Landing>
+    </div>
+  );
+}
+
+export default App;
